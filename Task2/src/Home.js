77635605893d3d554184555fd95d25cf6acf3645
Tanmay <tanmaymore06@gmail.com>
2021-09-17 15:54:36 +0530

    update

--- Task2/src/Home.js
+++ Task2/src/Home.js
@@ -5,22 +5,46 @@ const Home = () => {
     const onclick = async () => {
         const response = await fetch("https://reqres.in/api/users?page=1");
         const jsonresponse = await response.json();
-         setUser(jsonresponse.data);
-       //ok 
+        setUser(jsonresponse.data);
+        //ok 
     };
     return (
-        <div className="App">
-            <h1>Hello All</h1>
-            <button onClick={onclick}>Get Users</button>
-            <h2>Users:</h2>
-        
-                {users.map(({id,first_name})=>{
-                    return(
-                 <h3 key={id}>Name of the person is {first_name}</h3>
-                    );
-                    })}
-    
+        <div className="Home">
+            <div className="nav">
+                <nav>
+                    <div>
+                        <h2>JB Tours & Travels</h2>
+                    </div>
+                    <div>
+                        <button onClick={onclick}>Get Users</button>
+                    </div>
+                </nav>
+            </div>
+            <div className="usrcard">
+                {users.map(({ id, first_name, last_name, avatar, email }) => {
+                    return (
+                        <div className="card">
+                            <div className="pic">
+                                <img src={avatar}></img>
+                            </div>
 
+                            <div className="details">
+                                <h3>
+                                    {first_name} {last_name}
+                                </h3>
+                                <p>{email}</p>
+                            </div>
+                            <div className="social">
+                                <h3>follow me on</h3>
+                                <a href="#" class="fa fa-instagram"></a>
+                                <a href="#" class="fa fa-facebook"></a>
+                                <a href="#" class="fa fa-twitter"></a>
+                            </div>
+                        </div>
+                    );
+                })}
+
+            </div>
         </div>
     );
 
